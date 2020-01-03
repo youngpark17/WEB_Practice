0d5e451b0d7cad6fbe7f83ec3500c2d23f11ca80
YUP <mduddns@naver.com>
2020-01-03 22:54:49 +0900

    생활코딩 jquery를 사용해서 colors.js 파일 변경 후 적용

--- colors.js
+++ colors.js
@@ -1,22 +1,27 @@
-var Links = {setColor:function LinksSetColor(color){
-      var alist = document.querySelectorAll('a');
-      var i = 0;
-      while(i<alist.length){
-        alist[i].style.color=color;
-        i++;
-      }
-    }}
+var Links = {
+    setColor:function(color){
+      // var alist = document.querySelectorAll('a');
+      // var i = 0;
+      // while(i<alist.length){
+      //   alist[i].style.color=color;
+      //   i++;
+      // }
+      //이 웹페이지에 있는 모든a태그를 jquery로 제어하겠다.
+      $('a').css('color',color);
+    }
+  }
 
 var Body = {
-  setColor:function BodySetColor(color){
-    var target = document.querySelector('body');
-    target.style.color = color;
-  }, setBackgroundColor:  function bodySetBackgroundColor(color){
-      document.querySelector('body').style.backgroundColor=color;
-    }
+  setColor:function(color){
+    //var target = document.querySelector('body');
+    //target.style.color = color;
+    $('body').css('color',color);
+  },
+  setBackgroundColor: function(color){
+    //document.querySelector('body').style.backgroundColor=color;
+    $('body').css('backgroundColor',color);
+  }
 }
-
-
 
   function nightDayHandler(self){
     var target = document.querySelector('body');
